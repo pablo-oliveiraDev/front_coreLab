b62461cd5ed4ffc7cf987a3fcd3f45406b891475
pablo-oliveiraDev <pabloliverfe@gmail.com>
2024-01-31 12:55:02 -0300

    Fix:corrigido o erro de acesso ao cors

--- src/contexts/StoreNotesProviders.tsx
+++ src/contexts/StoreNotesProviders.tsx
@@ -1,55 +1,49 @@
 'use client';
 import { createContext, useEffect, useState } from 'react';
 import {
-  UserContextProps,
-  InitialValue
+    UserContextProps,
+    InitialValue
 } from '@/components/Types/contextTypes';
 import Api from '@/components/Services/api';
 
 export const StoreNotesContext = createContext<InitialValue>(
-  {} as InitialValue
+    {} as InitialValue
 );
 
 export const StoreNotesProvider = ({ children }: UserContextProps) => {
-  const [Loading, SetLoading] = useState<boolean>(false);
-  const [User, SetUser] = useState<Object>({});
+    const [Loading, SetLoading] = useState<boolean>(false);
+    const [User, SetUser] = useState<Object>({});
 
-  async function Login(email: string, password: string) {
-    let NewData = {
-      email: email,
-      password: password
-    };
+    async function Login(email: string, password: string) {
+        let NewData = {
+            email: email,
+            password: password
+        };
 
-    try {
-      SetLoading(true);
-      await Api.post('/login', NewData).then(res => {
-        // let DataUser = {
-        //   id: res.data[0].id,
-        //   userName: res.data.userName,
-        //   email: res.data.email,
-        //   password: res.data.password,
-        //   createdAt: res.data.createdAt
-        // };
-        SetUser(res.data);
-      });
-    } catch (err) {
-      console.log('erro login :' + err);
-    } finally {
-      SetLoading(false);
+        try {
+            SetLoading(true);
+            await Api.post('/login', NewData).then(res => {
+                SetUser(res.data);
+            });
+        } catch (error: any) {
+            console.log('context error: ' + error + User.msg);
+            SetUser('');
+        } finally {
+            SetLoading(false);
+        }
     }
-  }
 
-  console.log(User);
-  return (
-    <StoreNotesContext.Provider
-      value={{
-        Loading,
-        Login
-      }}
-    >
-      {children}
-    </StoreNotesContext.Provider>
-  );
+    console.log(User);
+    return (
+        <StoreNotesContext.Provider
+            value={{
+                Loading,
+                Login
+            }}
+        >
+            {children}
+        </StoreNotesContext.Provider>
+    );
 };
 
 export default StoreNotesProvider;
